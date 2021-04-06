63436c31d665993786ce5ce65eca273a4c08baf2
chienleow <chienleow1@gmail.com>
2021-04-05 22:09:14 -0400

    get mouse position

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const canvas = document.getElementById("canvas1");
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let particlesArray;
+
+// get mouse position
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80);
+}
+
